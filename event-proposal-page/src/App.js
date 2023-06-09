53966c7bf00003c67ff8463f2a28831014eab6b8
Himanshu Dahare <himanshudahare6400@gmail.com>
2023-06-09 11:42:17 +0530

    Create Vendor Structure

--- event-proposal-page/src/App.js
+++ event-proposal-page/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import CreateProposal from "./components/proposals/createProposal"
+
 
 function App() {
   return (
     <div className="App">
-      <CreateProposal/>
+      </>
     </div>
   );
 }
