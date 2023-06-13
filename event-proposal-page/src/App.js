cb6cb91b3f9fbd95816dd111384c76f1424c4fcf
Himanshu Dahare <himanshudahare6400@gmail.com>
2023-06-13 09:28:08 +0530

    Add Proposal Files

--- event-proposal-page/src/App.js
+++ event-proposal-page/src/App.js
@@ -1,12 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
-
+import Vencreateprop from "../src/components/Vendor/VendorCeateProp/Vencreateprop";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Vendorreg from "../src/components/Vendor/Vendorreg";
+import Userreg from "./components/User/Userreg";
+import Navbar from "./components/Vendor/VendorProp/Navbar";
+import VendorProp from "../src/components/Vendor/VendorProp/VendorProp";
+import Userhome from "../src/components/User/UserHome"
+import Vendorhome from "../src/components/Vendor/Vendorhome"
+import Userprop from "./components/User/UserProposals/Userprop";
+import Proposaldetail from "./components/User/UserProposals/Proposaldetail";
+import AppContext from "./components/User/UserProposals/Context"
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+    <AppContext>
+      <Routes>
       
-    </div>
+          <Route path="/createvendoraccount" element={<Vendorreg/>}/>
+          <Route path="/VendorProposal" element={<VendorProp/>}/>
+          <Route path='/createproposal' element={<Vencreateprop/>}/>
+          <Route path="/" element={<Vendorhome />} />
+          <Route path="/User" element={<Userhome/>}/>
+          <Route path="/createuseraccount" element={<Userreg/>}/>
+          <Route path='/Userproposals' element={<Userprop/>}/>
+          <Route path="/Proposaldetail/:id" element={<Proposaldetail/>}/>
+         
+          
+          
+      </Routes>
+      </AppContext>
+    
+    </BrowserRouter>
   );
 }
 
---
+++ event-proposal-page/src/components/User/UserProposals/UserNav.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Userprop.css";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function UserNav() {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.removeItem("userToken");
+    localStorage.removeItem("userloggedIn");
+    navigate("/User");
+  };
+  return (
+    <div className="nav">
+      <div className="venlogo">
+        <b>LOGO</b>
+      </div>
+  
+      <div className="name">
+        <b>{localStorage.getItem("userName")}</b>
+      </div>
+      <div className="dropdown">
+        {/**class */}
+        <div className="imgven"></div>
+        <div className="dropdown-content">
+          {/**class */}
+          <Link onClick={logout} to="/User">Log Out</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserNav;
